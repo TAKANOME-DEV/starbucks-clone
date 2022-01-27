6783cf299fdea2359cb95ca3b20063bc133182c5
takanome_dev <takanomedev221@gmail.com>
2022-01-27 10:37:34 +0000

    [feat]: add accordion to footer links

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const accordion = document.querySelectorAll(".acc");
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    const li = this.nextElementSibling;
-    console.log(li.scrollHeight);
-    let height = li.style.maxHeight;
-    console.log({ height });
+    const ul = this.nextElementSibling;
 
-    if (height) height = null;
-    else height = `${li.scrollHeight}px`;
+    if (ul.style.display === "block") ul.style.display = "none";
+    else ul.style.display = "block";
+
+    if (ul.style.height) ul.style.height = null;
+    else ul.style.height = `${ul.scrollHeight}px`;
   });
 }
