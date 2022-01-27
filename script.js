b3fc0ce44abf48f1b6c75fe7e67a4e1d36ab2dd0
takanome_dev <takanomedev221@gmail.com>
2022-01-27 18:31:14 +0000

    [doc]: add responsive design in header

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const accordion = document.querySelectorAll(".acc");
+const icon = document.querySelector(".icon");
+const link = document.querySelector(".links");
+const main = document.querySelector(".main");
+const body = document.querySelector(".body");
 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
@@ -12,3 +16,14 @@ for (let i = 0; i < accordion.length; i++) {
     else ul.style.height = `${ul.scrollHeight}px`;
   });
 }
+
+icon.addEventListener("click", function () {
+  this.classList.toggle("open");
+  link.classList.toggle("open");
+  main.classList.toggle("open");
+  body.classList.toggle("open");
+});
+
+// link.addEventListener("click", function () {
+//   this.classList.toggle("open");
+// });
